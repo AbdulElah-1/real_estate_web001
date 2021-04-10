28cb479a355f53de92e66521e4111242fa1b0435
Abdul Elah <elah.balagh@yahoo.ca>
2021-04-10 09:34:46 -0400

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -155,3 +155,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ASDF@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+
+try:
+    from .local_settings import * 
+except ImportError:
+    pass
+
